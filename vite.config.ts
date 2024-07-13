f5d90df1de0717585295d38322487c191415e1f6
Yashveer <yashveer@uixlabs.in>
2024-07-13 13:49:14 +0530

    tanstac router

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import TanStackRouterVite from "@tanstack/router-plugin/vite";
+import viteReact from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [TanStackRouterVite(), viteReact()],
+});
