501f3728b74dd1d19bb94f6fe245dddc759f5cf2
Yashveer <yashveer@uixlabs.in>
2024-07-14 16:00:31 +0530

    added light and dark theme

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,12 +1,22 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
 
+import { useTheme } from "../contexts/useTheme";
+
 export const Route = createLazyFileRoute("/")({
   component: Index,
 });
 
 function Index() {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="p-2">
+    <div className={`App ${theme}`}>
+      <header className="App-header">
+        <h1>React App with Tailwind CSS</h1>
+        <button className="btn-toggle" onClick={toggleTheme}>
+          Toggle {theme === "light" ? "Dark" : "Light"} Mode
+        </button>
+      </header>
       <h3>Welcome Home!</h3>
     </div>
   );
